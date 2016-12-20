b726a3b1546b7cd6ffb86275a6ae84c959654497
Richard Smiley <smiley@callrail.com>
2016-12-19 20:22:29 -0500

    day 6

---
+++ 6-2.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require 'set'
+
+first = true
+$char_pos_counts = []
+
+File.readlines("6.data").each_with_index do |line, i|
+  line.chars.each_with_index do |ch, i|
+    $char_pos_counts << Hash.new(0) if first
+    $char_pos_counts[i][ch] += 1
+  end
+  first = false
+end
+
+val = $char_pos_counts.map { |ct| ct.min_by{|k,v| v}[0] }
+puts val.join('')
